032301d9103e0fbe407759b8229699f37411d8f3
Spandana <spandyb99@gmail.com>
2022-07-19 23:05:30 +0530

    implemented user model

--- db/db-models/user.js
+++ db/db-models/user.js
@@ -2,7 +2,21 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
     {
-       
+        email: {
+            type: String,
+            required: true,
+            validate: {
+                validator: function(v) {
+                  return /.+\@.+\..+/.test(v);
+                },
+                message: props => `${props.value} is not a valid Email !`
+            },
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     }
 
 );
