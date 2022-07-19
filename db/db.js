019c609b5d9e6cb32828f30495d35f9f706690b9
Spandana <spandyb99@gmail.com>
2022-07-19 23:02:11 +0530

    Added db folder and db-models folders with intial db config file and user.js file

---
+++ db/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const conn_string = "mongodb+srv://Thanoojha:Thanu99@cluster0.hg7wk.mongodb.net/?retryWrites=true&w=majority"
+
+function connectToDb() {
+    return mongoose.connect(conn_string, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then((conn) => {
+        return conn;
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+connectToDb();
+
+module.exports = {
+    mongoose: mongoose,
+    User: require("./db-models/user"),
+    Quote: require("./db-models/quote"),
+    Profile:require("./db-models/profile"),
+    connectToDb: connectToDb,
+};
